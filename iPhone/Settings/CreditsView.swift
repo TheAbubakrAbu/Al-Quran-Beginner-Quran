466144c7b966e4fbe018abac7b45dcabdaf85102
TheAbubakrAbu <ammelmallah@icloud.com>
2025-03-02 18:43:29 -0800

    Version 1.3.5

--- iPhone/Settings/CreditsView.swift
+++ iPhone/Settings/CreditsView.swift
@@ -53,7 +53,7 @@ struct CreditsView: View {
             }
             
             Section {
-                Text("Version 1.3.4")
+                Text("Version 1.3.5")
                     .font(.caption)
             }
             

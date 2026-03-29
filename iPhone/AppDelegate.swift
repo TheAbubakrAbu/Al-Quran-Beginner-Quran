7b00baa21b80578d1a34a60fb6c6a0cb1eb738c5
elmallaa <elmallaa@uci.edu>
2026-03-29 16:27:18 -0700

    Version 2.3.0

--- iPhone/AppDelegate.swift
+++ iPhone/AppDelegate.swift
@@ -1,5 +1,4 @@
 #if os(iOS)
-import BackgroundTasks
 import UIKit
 import UserNotifications
 
@@ -17,14 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
 
         ReciterDownloadManager.shared.backgroundSessionCompletionHandler(completionHandler)
-    }
-
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        completionHandler([.banner, .sound])
     }
 }
 #endif
